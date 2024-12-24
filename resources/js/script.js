555abdb73b553cdd59bb47d02b885c90d2305320
pratik1243 <87714664+pratik1243@users.noreply.github.com>
2024-12-25 02:34:07 +0530

    Update script.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,6 +12,34 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
+var swiper = new Swiper(".mySwiper-md", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".mySwiper-sm", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
 let dropdownbtns = document.querySelectorAll(".dropdown-btn");
 dropdownbtns.forEach((btns) => {
   btns.addEventListener("click", function (event) {
@@ -23,3 +51,4 @@ let menuBtn = document.querySelector('.menu-btn');
 menuBtn.addEventListener("click", function (event) {
   document.querySelector('.responsive-dropdown-sec').classList.toggle("collapse1");
 });
+
