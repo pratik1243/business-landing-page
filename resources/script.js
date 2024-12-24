6a783175a9755bcc5054715788ebcbc1b1b92707
pratik1243 <87714664+pratik1243@users.noreply.github.com>
2024-12-24 15:04:58 +0530

    Add files via upload

---
+++ resources/script.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+let dropdownbtns = document.querySelectorAll(".dropdown-btn");
+dropdownbtns.forEach((btns) => {
+  btns.addEventListener("click", function (event) {
+    document.querySelector(`.sub-dropdown-list[data-id=${event.currentTarget.dataset.id}]`).classList.toggle("collapse");
+  });
+});
+
+let menuBtn = document.querySelector('.menu-btn');
+menuBtn.addEventListener("click", function (event) {
+  document.querySelector('.responsive-dropdown-sec').classList.toggle("collapse1");
+});
