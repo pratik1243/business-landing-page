97123b9fc361c58bbceb3e0601c93ec63214c227
pratik1243 <87714664+pratik1243@users.noreply.github.com>
2025-03-27 14:54:57 +0530

    Update script.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,6 +2,9 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
   loop: true,
+  autoplay: {
+    delay: 2000,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -10,14 +13,17 @@ var swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  observer: true,  
+  observer: true,
   observeParents: true,
 });
 
-var swiper = new Swiper(".mySwiper-md", {
+var swiper2 = new Swiper(".mySwiper-md", {
   slidesPerView: 2,
   spaceBetween: 30,
   loop: true,
+  autoplay: {
+    delay: 2000,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -26,14 +32,17 @@ var swiper = new Swiper(".mySwiper-md", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  observer: true,  
+  observer: true,
   observeParents: true,
 });
 
-var swiper = new Swiper(".mySwiper-sm", {
+var swiper3 = new Swiper(".mySwiper-sm", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
+  autoplay: {
+    delay: 2000,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -42,19 +51,24 @@ var swiper = new Swiper(".mySwiper-sm", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  observer: true,  
+  observer: true,
   observeParents: true,
 });
 
 let dropdownbtns = document.querySelectorAll(".dropdown-btn");
 dropdownbtns.forEach((btns) => {
   btns.addEventListener("click", function (event) {
-    document.querySelector(`.sub-dropdown-list[data-id=${event.currentTarget.dataset.id}]`).classList.toggle("collapse");
+    document
+      .querySelector(
+        `.sub-dropdown-list[data-id=${event.currentTarget.dataset.id}]`
+      )
+      .classList.toggle("collapse");
   });
 });
 
-let menuBtn = document.querySelector('.menu-btn');
+let menuBtn = document.querySelector(".menu-btn");
 menuBtn.addEventListener("click", function (event) {
-  document.querySelector('.responsive-dropdown-sec').classList.toggle("collapse1");
+  document
+    .querySelector(".responsive-dropdown-sec")
+    .classList.toggle("collapse1");
 });
-
